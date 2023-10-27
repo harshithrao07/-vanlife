7fcc8af9e312c85e8405a6f78e6058559177aa5a
Harshith Rao <harshithrao316@gmail.com>
2023-10-28 01:44:32 +0530

    test

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function About() {
     return(
         <main className="about--main">
-            <img className="about--image" src="../public/images/about.jpg" />
+            <img className="about--image" src="/images/about.jpg" />
             <div className="about--texts">
                 <h1>Don’t squeeze in a sedan when you<br />could relax in a van.</h1>
                 <p>Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch.
